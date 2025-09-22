12b0ce0a213d5dcc4fbaa89dd04cbd7374ca137e
Christina <christinaraganit@gmail.com>
2025-09-22 10:05:53 -0700

    test build

--- visionary/next.config.mjs
+++ visionary/next.config.mjs
@@ -4,6 +4,7 @@ const nextConfig = {
     // loader: "custom",
     // loaderFile: "./ImageLoader.js",
   },
+  output: "export",
 };
 
 /* https://christinaraganit.github.io/eric-dam.jpg?w=100&q=75 */
